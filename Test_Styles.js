923c243a1b2a03969f9b952c49c7527d5f8384ba
TroupXS2 <67026308+TroupXS2@users.noreply.github.com>
2021-12-11 08:26:57 +1000

    Updated Password System

--- Test_Styles.js
+++ Test_Styles.js
@@ -8,15 +8,15 @@
     window.alert("Enter Password")
     var ask1 = prompt("Please enter your username");
     var ask2 = prompt("Please enter your password");
-    var username = ("jwang141")
-    var username2 = ("Brill")
-    var password = ("Super")
+    var username = ("")
+    var username2 = ("")
+    var password = ("")
     if (ask1 == (username || username2) && ask2 == password) {
         window.alert("Password Accepted")
         document.getElementById("Sky").style.display = "block";
     }
     else{
-        window.alert("hihi")
+        window.alert("Error")
     }
   }
   function Keydown() {
